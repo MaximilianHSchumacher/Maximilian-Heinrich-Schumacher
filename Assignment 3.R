d2afac8594473f9fce08baff57786ce158effa57
MaximilianHSchumacher <maximilianhe@umass.edu>
2025-10-23 16:03:15 -0400

    First successful GitHub connection

--- Assignment 3.R
+++ Assignment 3.R
@@ -5,6 +5,10 @@ gitcreds::gitcreds_set()
 # (2) Git konfigurieren (Name & Mail wie auf GitHub)
 usethis::use_git_config(user.name = "MaixmilianHSchumacher",
                         user.email = "maximilianhe@umass.edu")
+system('git config --global user.name "MaximilianHSchumacher"')
+system('git config --global user.email "maximilianhe@umass.edu"')
+system('git config --global credential.helper manager')
+
 
 # (3) Optional: Ein neues GitHub-Repo direkt aus RStudio heraus erstellen
 use_github(private = TRUE)   # oder FALSE für öffentlich
